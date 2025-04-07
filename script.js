38c6e82a5bae38d6aea31bee1d02880e123bc1c5
fabricioMarquez444 <mm0341042021@unab.edu.sv>
2025-04-07 17:36:32 -0600

    2

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.getElementById("pesoForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    const peso = parseFloat(document.getElementById("peso").value);
-    const imagen = document.getElementById("img");
-    const audio = document.getElementById("sonido");
-  
-    if (peso > 67) {
-      imagen.classList.remove("oculto");
-      audio.play();
-    } else {
-      imagen.classList.add("oculto");
-      audio.pause();
-      audio.currentTime = 0;
-    }
-  });
-  
+  event.preventDefault();
+
+  const peso = parseFloat(document.getElementById("peso").value);
+  const imagen = document.getElementById("imagen");
+  const audio = document.getElementById("audio");
+
+  if (peso > 67) {
+    imagen.classList.remove("oculto");
+    audio.play();
+  } else {
+    imagen.classList.add("oculto");
+    audio.pause();
+    audio.currentTime = 0;
+  }
+});
